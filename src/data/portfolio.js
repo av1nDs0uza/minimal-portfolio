7132aefa4af73c4019bc2642a3914fe5d57bb6e7
av1nDs0uza <avinalricdsouza@gmail.com>
2024-03-23 12:44:41 +0530

    images added

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,26 +1,26 @@
 export default [
     {
        title: 'Habit tracker',
-       imgUrl: '/assets/terminal-website.jpg',
+       imgUrl: './src/assets/habit-tracker.png',
        stack: ['Flutter', 'Firebase'],
        link: 'https://github.com/av1nDs0uza/habit-tracker',
     },
     {
        title: 'ASL recogniser',
-       imgUrl: '/assets/portfolio.JPG',
+       imgUrl: './src/assets/sign-language.png',
        stack: ['Python', 'Machine Learning', 'DeepLearning', 'TensorFlow'],
        link: 'https://github.com/av1nDs0uza/LSTm-Action-Detection',
     },
     {
        title: 'Smart bank locker system',
-       imgUrl: '/assets/smart-bank-locker.png',
+       imgUrl: './src/assets/smart-bank-locker.png',
        stack: ['Python', 'php', 'IOT','Machine Learning', 'Arduino','Raspberrypi'],
        link: 'https://clevervault.tech/door/',
     },
     {
       title: 'Portfolio website',
-      imgUrl: '/assets/smart-bank-locker.png',
+      imgUrl: './src/assets/portfolio.JPG',
       stack: ['React', 'tailwindcss', 'vite'],
-      link: 'https://clevervault.tech/door/',
+      link: 'https://avindsouza.in/',
    },
  ]
