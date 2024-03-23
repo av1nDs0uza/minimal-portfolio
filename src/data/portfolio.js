c1bce1bae4243d48bab10f6c796441754205a564
av1nDs0uza <avinalricdsouza@gmail.com>
2024-03-23 14:49:44 +0530

    shouldn't have created public folder

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,25 +1,25 @@
 export default [
     {
        title: 'Habit tracker',
-       imgUrl: './src/public/habit-tracker.png',
+       imgUrl: '/habit-tracker.PNG',
        stack: ['Flutter', 'Firebase'],
        link: 'https://github.com/av1nDs0uza/habit-tracker',
     },
     {
        title: 'ASL recogniser',
-       imgUrl: './src/public/sign-language.png',
+       imgUrl: '/sign-language.png',
        stack: ['Python', 'Machine Learning', 'DeepLearning', 'TensorFlow'],
        link: 'https://github.com/av1nDs0uza/LSTm-Action-Detection',
     },
     {
        title: 'Smart bank locker system',
-       imgUrl: './src/public/smart-bank-locker.png',
+       imgUrl: '/smart-bank-locker.png',
        stack: ['Python', 'php', 'IOT','Machine Learning', 'Arduino','Raspberrypi'],
        link: 'https://clevervault.tech/door/',
     },
     {
       title: 'Portfolio website',
-      imgUrl: './src/public/portfolio.JPG',
+      imgUrl: '/portfolio.JPG',
       stack: ['React', 'tailwindcss', 'vite'],
       link: 'https://avindsouza.in/',
    },
