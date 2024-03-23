52af13b13989c5799970bf0802a2933c89ea7049
av1nDs0uza <avinalricdsouza@gmail.com>
2024-03-23 14:35:12 +0530

    changed "assets" to "public

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,25 +1,25 @@
 export default [
     {
        title: 'Habit tracker',
-       imgUrl: './src/assets/habit-tracker.png',
+       imgUrl: './src/public/habit-tracker.png',
        stack: ['Flutter', 'Firebase'],
        link: 'https://github.com/av1nDs0uza/habit-tracker',
     },
     {
        title: 'ASL recogniser',
-       imgUrl: './src/assets/sign-language.png',
+       imgUrl: './src/public/sign-language.png',
        stack: ['Python', 'Machine Learning', 'DeepLearning', 'TensorFlow'],
        link: 'https://github.com/av1nDs0uza/LSTm-Action-Detection',
     },
     {
        title: 'Smart bank locker system',
-       imgUrl: './src/assets/smart-bank-locker.png',
+       imgUrl: './src/public/smart-bank-locker.png',
        stack: ['Python', 'php', 'IOT','Machine Learning', 'Arduino','Raspberrypi'],
        link: 'https://clevervault.tech/door/',
     },
     {
       title: 'Portfolio website',
-      imgUrl: './src/assets/portfolio.JPG',
+      imgUrl: './src/public/portfolio.JPG',
       stack: ['React', 'tailwindcss', 'vite'],
       link: 'https://avindsouza.in/',
    },
