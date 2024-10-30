03d83a4a6100b78d7f06957a9b69c2e08f5f9726
Avin Dsouza <59828163+av1nDs0uza@users.noreply.github.com>
2024-10-30 12:56:56 +0530

    Update PortfolioItem.jsx

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -6,25 +6,28 @@ function PortfolioItem({ title, imgUrl, stack, link }) {
          href={link}
          target="_blank"
          rel="noopener noreferrer"
-         className="border-2 border-stone-900 dark:border-white rounded-md overflow-hidden"
+         className="border-4 border-black dark:border-white rounded-lg overflow-hidden transition-shadow duration-200 hover:shadow-lg"
       >
          <img
             src={imgUrl}
-            alt="portfolio" 
-            className="w-full h-36 md:h-48 object-cover cursor-pointer"
+            alt={title} 
+            className="w-full h-36 md:h-48 object-cover cursor-pointer border-b-4 border-black dark:border-white"
          />
-         <div className="w-full p-4">
-            <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold ">{title}</h3>
-            <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white ">
-               {stack.map(item => (
-                  <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md">
+         <div className="w-full p-4 bg-gray-100 dark:bg-gray-800">
+            <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-bold">{title}</h3>
+            <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white">
+               {stack.map((item, index) => (
+                  <span 
+                     key={index}
+                     className="inline-block px-2 py-1 font-semibold border-2 border-black dark:border-white rounded-md bg-white dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors duration-200"
+                  >
                      {item}
                   </span>
                ))}
             </p>
          </div>
       </a>
-   )
+   );
 }
 
 export default PortfolioItem;
